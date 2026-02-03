597ccd56e8dd7092e195bedff3d5b5a3eba9d778
Nico Castro <castroconi101@gmail.com>
2026-02-02 21:25:51 -0500

    Added some changes on Contact

--- client/app/components/MainHeader.tsx
+++ client/app/components/MainHeader.tsx
@@ -33,22 +33,24 @@ export default function MainHeader(){
         About
       </a>
 </div>
-     <div className="w-28 m-2 border-gray-400 border-2 rounded-2xl flex items-center justify-center ">
- <a
+<div className="flex mr-6">
+ <div className="flex rounded-b mb-2 bg-gradient-to-tr from-green-400 to-slate-300 p-7 text-lg justify-center items-center mr-4"> 
+        <a
         href="#contacts"
-        className="text-lg text-gray-600 font-bold"
+        className="text-white font-bold"
       >
           CONTACT
         </a>
 </div>
-<div>
+         <div className="flex bg-gradient-to-t from-blue-950 to-gray-900 px-7 text-lg justify-center items-center"> 
         <a
           href="/consultation"
-          className="text-black"
+          className="text-white"
         >
           Get Consultation
         </a>
         </div>
+</div>
 
     </div>
     )
